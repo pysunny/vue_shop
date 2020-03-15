8449cca48f9414e98f813e39b504217f3a16191f
sunny <120906209@qq.com>
2020-03-15 22:37:17 +0800

    用户组件 2020-03-15

---
+++ src/components/user/Users.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- 面包屑显示区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-row :gutter="20">
+      <!-- 搜索与添加 -->
+        <el-col :span="7">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
